872046df3a7f2cb548dc489be6509d1fe9aaa2c3
Lu Murga <lulipsicounc@gmail.com>
2021-11-09 21:22:39 -0300

    Proxy service enable and common features corrections

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 
+
+
 window.addEventListener('load', (e) => {
   let
   qs = element => document.querySelector(element),
@@ -18,16 +20,24 @@ window.addEventListener('load', (e) => {
     formGo.addEventListener('submit', (e) => {
         e.preventDefault(); 
         
-        let name = inputName.value.split(" ", 1);  
-        console.log(name);
-        fetch(`https://bio.torre.co/api/bios/${name}`)
-        .then((resp) => resp.json())
-        
+        let userName = inputName.value.split(" ", 1);  
+        let proxy = "https://cors-everywhere-me.herokuapp.com/";
+        let url =  proxy + `https://bio.torre.co/api/bios/${userName}`;
+
+        fetch(url)
+        .then(response => {
+        if(response.status != 200){
+        console.log(`Error: ${response.status}`)
+        }else{
+           return response.json()
+         }
+      })
+    
         .then((data) => {
             
             //Person data is renderized
             user.innerHTML = `<div class="userPhoto">
-                                 <img src="${data.person.picture}" alt="user´s photography">
+                                 <img src="${data.person.picture==undefined?"https://image.freepik.com/free-vector/404-error-page-found_24908-59519.jpg":data.person.picture}" alt="user´s photography">
                                  <h2>${data.person.name}</h2>
                                  <h4>${data.person.professionalHeadline}</h4>
                                  <h4>${data.person.location.country}</h4>
@@ -36,8 +46,10 @@ window.addEventListener('load', (e) => {
             formGo.reset();
             
             //Skills are renderized
-            let skills = data.person.strengths;
+            let skills = data.strengths;
             skills.map((skill) => {
+
+
               if(skill.proficiency==="proficient"){
                 
                 return skillP.innerHTML+=`
@@ -73,16 +85,16 @@ window.addEventListener('load', (e) => {
             })
 
             //Languages are renderized          
-            let languages = data.person.languages;
+            let languages = data.languages;
             languages.map((language) => {
               lang.innerHTML += `
-                                <li>${language.language} (${language.code}). Fluency: ${language.fluency}</li>
+                                <li>${language.language} (${language.code})</li>
                                 
                                    `
             })  
             
             //Industries and interests are renderized
-            let interests = data.person.interests;
+            let interests = data.interests;
             interests.map((interest) => {
               ind.innerHTML += `
                                <li>${interest.name}</li>
